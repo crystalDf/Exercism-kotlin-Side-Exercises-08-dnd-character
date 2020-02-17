06dc0012f91044ff671e1ab484edee9dec07f42e
crystalDf <chendong333@gmail.com>
2020-02-17 23:50:12 +0800

    08 dnd-character

---
+++ src/main/kotlin/DndCharacter.kt
@@ -0,0 +1,24 @@
+import kotlin.math.floor
+
+class DndCharacter {
+
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = 10 + modifier(constitution)
+
+    companion object {
+
+        fun ability(): Int {
+            return generateSequence { (1..6).shuffled().first() }.take(4)
+                    .sortedDescending().take(3).sum()
+        }
+
+        fun modifier(score: Int): Int {
+            return floor((score - 10) / 2.0).toInt()
+        }
+    }
+}
